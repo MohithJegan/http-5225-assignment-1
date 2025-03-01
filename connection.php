2848243cbf251b37380139c62698441ef8532c9b
Mohith J <mohithjegan@gmail.com>
2025-03-01 16:17:45 -0500

    Add the code

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$connect = mysqli_connect('sql107.infinityfree.com', 'if0_38425702', '6JfNUyyvX5c6v', 'if0_38425702_movies');
+
+if (!$connect) {
+  echo 'Error Code: ' . mysqli_connect_errno();
+  echo 'Error Message: ' . mysqli_connect_error();
+  exit;
+}
